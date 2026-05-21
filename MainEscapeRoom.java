cc5aecebe3145bbf3406c939188ca281a623e015
EllaChan-123 <ella.chan26@ycdsbk12.ca>
2026-05-21 13:47:26 -0400

    Update MainEscapeRoom.java
    Change and delete some variables to make it more flexible

--- MainEscapeRoom.java
+++ MainEscapeRoom.java
@@ -13,18 +13,17 @@ import javax.swing.event.*;
 
 public class MainEscapeRoom implements ActionListener{
 	JFrame theFrame = new JFrame("Escape Room");
-	JStartingScreen thePanel = new JStartingScreen();
 	Timer theTimer = new Timer(1000/60, this);
 	
+	//Starting Panel
+	
+	//Introduction Panel
+	
 	public void actionPerformed (ActionEvent evt){
-		thePanel.repaint();
+		
 	}
 	
 	public MainEscapeRoom(){
-		thePanel.setLayout(null);
-        thePanel.setPreferredSize(new Dimension(1280, 720));
-
-        theFrame.setContentPane(thePanel);
         theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         theFrame.pack();
         theFrame.setVisible(true);
