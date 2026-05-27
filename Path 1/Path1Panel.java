29c100d77e5dd93138722822e29bb0a572a985b0
EllaChan-123 <ella.chan26@ycdsbk12.ca>
2026-05-27 14:20:10 -0400

    Created csv file for path 1
    Added temporary clues and locations + all the items and the sequence

---
+++ Path 1/Path1Panel.java
@@ -0,0 +1,17 @@
+//Path 1 View
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+
+public class Path1Panel{
+	//Properties
+	
+	
+	//Methods
+	
+	
+	//Constructor
+
+}
